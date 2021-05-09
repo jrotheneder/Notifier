a2164022ce837c612f0ae7fd1d5f3bb95c81e8bb
Johannes Rotheneder <johannes.rotheneder@me.com>
2021-05-09 22:42:05 +0200

    Further fixes

--- tests/test_zara_product.py
+++ tests/test_zara_product.py
@@ -7,11 +7,12 @@ def test():
 
     url2 = "https://www.zara.com/at/en/comfort-flannel-trousers-p07380362.html?v1=84996070"
     url3 = "https://www.zara.com/at/en/share/viscose-and-linen-shirt---limited-edition-p04177015.html"
+    url4 = "https://www.zara.com/at/en/black-chelsea-boots-p12000720.html"
 
     zp = ZaraProduct.fromUrlSku(url2, "82273634")
     print(zp,"\n")
 
-    zp = ZaraProduct.fromUrlSize(url3, "S")
+    zp = ZaraProduct.fromUrlSize(url4, "43")
     print(zp, "\n")
 
     zp.dict["price"] = "over 9000"  
--- tests/test_zara_scraper.py
+++ tests/test_zara_scraper.py
@@ -6,11 +6,13 @@ import requests
 
 url2 = "https://www.zara.com/at/en/comfort-flannel-trousers-p07380362.html"
 url3 = "https://www.zara.com/at/en/leather-brogue-boots-p12020620.html"
+url4 = "https://www.zara.com/at/en/black-chelsea-boots-p12000720.html"
 
-url = url2
+url = url4
 
 lst = ZaraScraper.getProductList(url)
 print(ZaraScraper.skuSummary(lst))
-print(ZaraScraper.getProductFromSku(url, 82273636))
+#print(ZaraScraper.getProductFromSku(url, "82273636"))
+print(ZaraScraper.getProductFromSize(url, "43"))
 #print(json.dumps(ZaraScraper.getProductList(url), indent=4,
 #    sort_keys=True))
