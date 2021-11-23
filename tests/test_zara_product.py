dd35543fc646a339c96b01827d05edab03aa5bee
Johannes Rotheneder <johannes.rotheneder@me.com>
2021-11-23 23:56:20 +0100

    Update tests

--- tests/test_zara_product.py
+++ tests/test_zara_product.py
@@ -5,15 +5,15 @@ from zara_product import ZaraProduct
 
 def test(): 
 
-    url2 = "https://www.zara.com/at/en/comfort-flannel-trousers-p07380362.html?v1=84996070"
+    url2 = "https://www.zara.com/at/en/striped-shirt-p05571300.html?v1=123839133&v2=1886403"
     url3 = "https://www.zara.com/at/en/share/viscose-and-linen-shirt---limited-edition-p04177015.html"
     url4 = "https://www.zara.com/at/en/black-chelsea-boots-p12000720.html"
 
-    zp = ZaraProduct.fromUrlSku(url2, "82273634")
+    zp = ZaraProduct.fromUrlSku(url2, "123839133-622-2")
     print(zp,"\n")
 
-    zp = ZaraProduct.fromUrlSize(url4, "43")
-    print(zp, "\n")
+#   zp = ZaraProduct.fromUrlSize(url4, "43")
+#   print(zp, "\n")
 
     zp.dict["price"] = "over 9000"  
     print(zp) 
--- tests/test_zara_scraper.py
+++ tests/test_zara_scraper.py
@@ -4,15 +4,15 @@ sys.path.insert(1, os.path.join(sys.path[0], '..'))
 from zara_scraper import *
 import requests
 
-url2 = "https://www.zara.com/at/en/comfort-flannel-trousers-p07380362.html"
+url2 = "https://www.zara.com/at/en/striped-shirt-p05571300.html?v1=123839133&v2=1886403"
 url3 = "https://www.zara.com/at/en/leather-brogue-boots-p12020620.html"
 url4 = "https://www.zara.com/at/en/black-chelsea-boots-p12000720.html"
 
-url = url4
+url = url2
 
 lst = ZaraScraper.getProductList(url)
 print(ZaraScraper.skuSummary(lst))
-#print(ZaraScraper.getProductFromSku(url, "82273636"))
-print(ZaraScraper.getProductFromSize(url, "43"))
+print(ZaraScraper.getProductFromSku(url, "110483145"))
+#print(ZaraScraper.getProductFromSize(url, "M"))
 #print(json.dumps(ZaraScraper.getProductList(url), indent=4,
 #    sort_keys=True))
